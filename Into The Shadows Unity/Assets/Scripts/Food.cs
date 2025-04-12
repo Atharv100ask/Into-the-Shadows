0407bb5a9250f3207a8d66e5757eb4ac01db465e
Hieu <hcao32@uic.edu>
2025-04-11 23:11:37 -0500

    Added basic hotbar system and food pickup

--- Into The Shadows Unity/Assets/Scripts/Food.cs
+++ Into The Shadows Unity/Assets/Scripts/Food.cs
@@ -4,12 +4,15 @@ public class Food : MonoBehaviour
 {
 
     public HealthBar status;
+    public Inventory inventory;
     private void OnCollisionEnter (Collision collision)
     {
         Destroy(gameObject);
-        Debug.Log("Health: " + status.currentHealth);
-        status.currentHealth += 25f;
-        Debug.Log("Health: " + status.currentHealth);
-        status.UpdateHealthBar();
+        // Debug.Log("Health: " + status.currentHealth);
+        // status.currentHealth += 25f;
+        // Debug.Log("Health: " + status.currentHealth);
+        // status.UpdateHealthBar();
+        inventory.food += 1;
+        inventory.UpdateItemCount();
     }
 }
---
+++ Into The Shadows Unity/Assets/Scripts/Inventory.cs
@@ -0,0 +1,104 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Inventory : MonoBehaviour
+{
+    public Transform hotbarParent;
+    public TMP_Text foodAmount;
+    public TMP_Text stabilizerAmount;
+    public TMP_Text ammoAmount;
+    private RectTransform[] hotbarSlots;
+    public bool hasMelee;
+    public bool hasGun;
+    public bool hasFlashlight;
+    public int food;
+    public int stabilizers;
+    public int ammo;
+    private Color normalColor;
+    private Color highlight;
+    private float scale = 1.2f;
+
+
+    // 1: Melee, 2: Gun, 3: Food, 4: Stabilizers, 5: Ammo F: flashlight
+    void Start()
+    {
+        hasMelee = false;
+        hasGun = false;
+        hasFlashlight = false;
+        food = 0;
+        stabilizers = 0;
+        ammo = 0;
+        normalColor = new Color32(103,103,103,100);
+        highlight = new Color32(0,255,255,100);
+        hotbarSlots = new RectTransform[6];
+        for (int i = 0; i < 6; i++)
+        {
+            hotbarSlots[i] = hotbarParent.GetChild(i).GetComponent<RectTransform>();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Debug.Log("Pressed 1");
+            HighlightItem(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Debug.Log("Pressed 2");
+            HighlightItem(2);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Debug.Log("Pressed 3");
+            HighlightItem(3);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Debug.Log("Pressed 4");
+            HighlightItem(4);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            Debug.Log("Pressed 5");
+            HighlightItem(5);
+        }
+        else if (Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("Pressed F");
+            HighlightItem(6);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            Debug.Log("Pressed 6");
+            HighlightItem(6);
+        }
+    }
+
+    void HighlightItem(int key){
+        for (int i = 0; i < hotbarSlots.Length; i++)
+        {
+            var image = hotbarSlots[i].GetComponent<Image>();
+
+            if (i == (key - 1))
+            {
+                hotbarSlots[i].localScale = Vector3.one * scale;
+                if (image != null) image.color = highlight;
+            }
+            else
+            {
+                hotbarSlots[i].localScale = Vector3.one * 1;
+                if (image != null) image.color = normalColor;
+            }
+        }
+    }
+
+    public void UpdateItemCount()
+    {
+        foodAmount.text = food.ToString();
+        stabilizerAmount.text = stabilizers.ToString();
+        ammoAmount.text = ammo.ToString();
+    }
+}
